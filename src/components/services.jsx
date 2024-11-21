cbc14d0986fb9861d9d364389a63e545a17bcc7d
Frederic Desjardins <66186872+Fredotez@users.noreply.github.com>
2024-11-21 16:06:31 -0500

    Initial commit

---
+++ src/components/services.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+//<h3>{d.name}</h3>
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+            Our company offers a comprehensive range of services tailored to meet your year-round needs. From expert landscaping and moving assistance during the warmer months to reliable snow removal and other handyman tasks in the winter, we are dedicated to providing efficient and quality solutions for your home or business.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+              <div key={`${d.name}-${i}`} className="col-md-4">
+                {" "}
+                <i className={d.icon}></i>
+                <div className="service-desc">
+                  <br></br>
+                  <p>{d.text}</p>
+                </div>
+              </div>
+            ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
